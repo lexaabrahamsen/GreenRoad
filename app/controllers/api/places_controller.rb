e07895c684644e8a253f2895a59097d2f218e66f
Lexa <lexaraea@gmail.com>
2019-08-23 16:54:39 -0400

    searching by zip code working

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,8 +1,18 @@
 class Api::PlacesController < ApplicationController
-	def index
-		@places = Place.all
-		render "index.json.jb"
-	end
+	# def index
+	# 	@places = Place.all
+	# 	render "index.json.jb"
+	# end
+  def index
+    if params[:search]
+      @places = Place.where("distance LIKE ?", "%#{params[:search]}%")
+    else
+      @places = Place.all
+    end
+
+    @places = @places.order(:id => :asc)
+    render "index.json.jb"
+  end
 
 	def show
 		the_id = params[:id]
