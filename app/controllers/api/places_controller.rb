087b310d7ebe8316411aec25718cfc20e41d7196
Lexa <lexaraea@gmail.com>
2019-09-02 00:49:37 -0400

    Twilio working

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -3,15 +3,20 @@ class Api::PlacesController < ApplicationController
 	# 	@places = Place.all
 	# 	render "index.json.jb"
 	# end
+  # ***THIS WORKS FOR TWILIO***
   def index
-    Twilio.configure do |config|
-
-    end
+    # Twilio.configure do |config|
+    # config.account_sid = "ACea900c4ec6fed927d0379d6839d951d5"
+    # config.auth_token = "9ef6b306cf16fdaeae10ee16cfb1e7f5"
+    # end
     client = Twilio::REST::Client.new
     client.messages.create({
 
       body: 'Hello there! This is a test'
     })
+  # ADDING SECRETS YML
+  # ADDIND TWILIO.RB
+
     if params[:search]
       @places = Place.where("distance LIKE ?", "%#{params[:search]}%")
     else
